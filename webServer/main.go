44dd5b0ae337fa5da8429d8f7a3e243472a38b60
andreapassinialten <andrea.passini@alten.it>
2023-10-02 15:21:13 +0200

    Server

--- webServer/main.go
+++ webServer/main.go
@@ -10,15 +10,152 @@ import (
 	"time"
 )
 
+var movies map[string]Movie = make(map[string]Movie)
+
 func main() {
+
+	// Add using unique key and a value
+	movies["1"] = Movie{
+		ImdbID:   "1",
+		Title:    "Titanic",
+		Genre:    "Drama",
+		Director: "James Cameron",
+	}
+
+	movies["2"] = Movie{
+		ImdbID:   "2",
+		Title:    "Terminator",
+		Genre:    "Sci-fi",
+		Director: "James Cameron",
+	}
 
 	fmt.Println("Starting a Web Server")
 
 	http.HandleFunc("/hello", HelloHandler)
 	http.HandleFunc("/actors", ActorHandler)
 	http.HandleFunc("/newReq", MyHandler)
+	http.HandleFunc("/movies", MovieHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil)) // We dont need to specify another handler, since we attach it before
+
+}
+
+type Movie struct {
+	ImdbID   string  `json:"imdb_id"`
+	Title    string  `json:"title"`
+	Genre    string  `json:"genre"`
+	Director string  `json:"director"`
+	Cast     []Actor `json:"cast"`
+}
+
+type Actor struct {
+	Name string `json:"name"`
+}
+
+type MovieError struct {
+	HappenedAt   time.Time `json:"happened_at"`
+	Error        error     `json:"error"`
+	ErrorMessage string
+}
+
+func MovieHandler(w http.ResponseWriter, r *http.Request) {
+	var method string = r.Method
+
+	switch method {
+	case http.MethodGet:
+		fmt.Println("Movies GET: ")
+		var body io.ReadCloser = r.Body
+		var movie Movie
+		_ = json.NewDecoder(body).Decode(&movie)
+
+		val, ok := movies[movie.ImdbID]
+
+		var movieResponse = val
+
+		if ok {
+			w.WriteHeader(http.StatusAccepted)
+			_ = json.NewEncoder(w).Encode(movieResponse)
+			fmt.Println(movieResponse)
+		} else {
+			w.WriteHeader(http.StatusNoContent)
+			fmt.Println("No movie found")
+			movieError := MovieError{
+				Error:        errors.New("No movie found"),
+				HappenedAt:   time.Now(),
+				ErrorMessage: "No movie found",
+			}
+			fmt.Println(movieError)
+			_ = json.NewEncoder(w).Encode(movieError)
+		}
+
+	case http.MethodPost:
+		fmt.Println("Movies POST: ")
+		var body io.ReadCloser = r.Body
+		var movie Movie
+		_ = json.NewDecoder(body).Decode(&movie)
+
+		val, ok := movies[movie.ImdbID]
+		if !ok {
+			movies[movie.ImdbID] = Movie{
+				ImdbID:   movie.ImdbID,
+				Title:    movie.Title,
+				Genre:    movie.Genre,
+				Director: movie.Director,
+				Cast:     movie.Cast,
+			}
+			w.WriteHeader(http.StatusAccepted)
+			_, _ = io.WriteString(w, "Movie added")
+			fmt.Println("Movie added")
+			fmt.Println(val)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Println("Cannot ADD movie")
+			movieError := MovieError{
+				Error:        errors.New("Cannot ADD movie"),
+				HappenedAt:   time.Now(),
+				ErrorMessage: "Cannot ADD movie",
+			}
+			fmt.Println(movieError)
+			_ = json.NewEncoder(w).Encode(movieError)
+		}
+
+	case http.MethodDelete:
+		fmt.Println("Movies DELETE: ")
+		var body io.ReadCloser = r.Body
+		var movie Movie
+		_ = json.NewDecoder(body).Decode(&movie)
+
+		delete(movies, movie.ImdbID)
+
+		_, ok := movies[movie.ImdbID]
+
+		if !ok {
+			w.WriteHeader(http.StatusAccepted)
+			_, _ = io.WriteString(w, "Movie deleted")
+			fmt.Println("Movie deleted")
+			fmt.Println(movie.ImdbID)
+		} else {
+			w.WriteHeader(http.StatusNoContent)
+			fmt.Println("No movie to delete")
+			movieError := MovieError{
+				Error:        errors.New("No movie to delete"),
+				HappenedAt:   time.Now(),
+				ErrorMessage: "No movie to delete",
+			}
+			fmt.Println(movieError)
+			_ = json.NewEncoder(w).Encode(movieError)
+		}
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		// Tell the client that there is an error
+		movieError := MovieError{
+			Error:      errors.New("Something went wrong"),
+			HappenedAt: time.Now(),
+		}
+		fmt.Println(movieError)
+		_ = json.NewEncoder(w).Encode(movieError)
+	}
 }
 
 func MyHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,8 +180,6 @@ func MyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	// _, _ = fmt.Fprint(w, "hello world")
-
 	method := r.Method
 
 	switch method {
