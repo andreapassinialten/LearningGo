4f01afddc63ef33be24c9895d0e01d88d8b7c695
andreapassinialten <andrea.passini@alten.it>
2023-10-03 09:03:50 +0200

    Handler 2

---
+++ Server3rdPartyLibraries/cmd/dependencies.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"Server3rdPartyLibraries/db"
+	"Server3rdPartyLibraries/internal/movie"
+	"Server3rdPartyLibraries/internal/movie/handler"
+	"Server3rdPartyLibraries/internal/movie/repository"
+)
+
+type Dependencies struct {
+	movieHandler *handler.Handler
+}
+
+func NewDependencies() *Dependencies {
+	movieDB, err := db.CreateDB()
+	if err != nil {
+		panic(err)
+	}
+
+	movieStorage := repository.NewRepo(movieDB)
+	movieService := movie.NewService(movieStorage)
+
+	movieHandler := handler.NewHandler(movieService)
+
+	return &Dependencies{movieHandler: movieHandler}
+}
--- Server3rdPartyLibraries/internal/movie/repository/repository.go
+++ Server3rdPartyLibraries/internal/movie/repository/repository.go
@@ -5,10 +5,20 @@ import (
 	"Server3rdPartyLibraries/internal/movie"
 )
 
-type Repository struct {
+type RepositorySQLITE struct {
 	db *db.MoviesDB
 }
 
-func (r Repository) CreateMovie(m movie.Movie) (movie.Movie, error) {
+type RepositoryMONGODB struct {
+	db *db.MoviesDB
+}
+
+func NewRepo(db *db.MoviesDB) RepositorySQLITE {
+	return RepositorySQLITE{
+		db: db,
+	}
+}
+
+func (r RepositorySQLITE) CreateMovie(m movie.Movie) (movie.Movie, error) {
 	return movie.Movie{}, nil
 }
